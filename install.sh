b2c413eba9f22bf96c72c2274decf70822800aeb
すずねーう <suzuneu@suzuneu.com>
2022-06-24 11:44:37 +0000

    cmtt

--- install.sh
+++ install.sh
@@ -2,42 +2,17 @@
 
 set -u
 
-THIS_DIR=$(cd $(dirname $0); pwd)
+#...
 
-if [[ ! "$CODESPACES" == true ]]; then
-  exit 0
-fi
+DOTPATH=~/.dotfiles
 
-cd $THIS_DIR
-git submodule init
-git submodule update
+echo "START"
 
-echo "start setup..."
-for f in .??*; do
+for f in .??*
+do
     [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitconfig.local.template" ] && continue
-    [ "$f" = ".require_oh-my-zsh" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
 
-    ln -snfv ~/dotfiles/"$f" ~/
+    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
 
-[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
-
-THIS_DIR=$(cd $(dirname $0); pwd)
-
-cd $THIS_DIR
-git submodule init
-git submodule update
-
-echo "start setup..."
-for f in .??*; do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitconfig.local.template" ] && continue
-    [ "$f" = ".require_oh-my-zsh" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
-
-    ln -snfv ~/dotfiles/"$f" ~/
-done
-
-[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
+echo "DONE"
